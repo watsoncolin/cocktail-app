8bdfab964e353e5fb55fa5f24a865ba599dd21ee
Colin Watson <colin.watson@gmail.com>
2019-02-08 20:30:45 -0700

    default project added

---
+++ ios/cocktail-app/cocktail-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocktail-app
+//
+//  Created by Colin Watson on 2/8/19.
+//  Copyright © 2019 Colin Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
